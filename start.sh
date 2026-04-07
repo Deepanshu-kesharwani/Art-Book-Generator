f842655fe68f70543dfce968772e63cadefc86b0
Deepanshu Kesharwani <dkesharwani2002@gmail.com>
2026-04-07 10:36:08 +0530

    delete file

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run FastAPI in background (internal use)
-uvicorn main:app --host 0.0.0.0 --port 8000 &
-
-# Run Streamlit on HF-required port
-streamlit run frontend/app.py \
-  --server.port 7860 \
-  --server.address 0.0.0.0
